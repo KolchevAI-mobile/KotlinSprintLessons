fb6c8ae2ffba9917ed2b37e75165bd4940f9c41d
KolchevAI <aleks.kolchev2004@gmail.com>
2025-12-01 14:17:00 +0300

    KSL-1-2 completed the second task

---
+++ src/main/kotlin/firstLesson/secondTask.kt
@@ -0,0 +1,18 @@
+package org.example.firstLesson
+
+fun main() {
+
+    var ordersCount: Int = 75
+    val thanksForOrder: String = "Thanks for your order!"
+
+    println(ordersCount)
+    println(thanksForOrder)
+
+    var employeeCount: Int = 2000
+
+    //println(employeeCount)
+
+    employeeCount = 1999
+
+    println(employeeCount)
+}
