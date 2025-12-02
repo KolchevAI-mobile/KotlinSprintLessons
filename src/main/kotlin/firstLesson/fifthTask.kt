4898b05a795039e22cbcae9066800ca23e51eb44
KolchevAI <aleks.kolchev2004@gmail.com>
2025-12-02 14:43:17 +0300

    KS-1-5 converted the magic number to const val and refactored string conversion

--- src/main/kotlin/firstLesson/fifthTask.kt
+++ src/main/kotlin/firstLesson/fifthTask.kt
@@ -1,15 +1,18 @@
 package org.example.firstLesson
 
-fun main() {
-    val seconds: Int = 6480
-    val minutes: Int = seconds / 60
-    val remainingSeconds: Int = seconds % 60
-    val hours = minutes / 60
-    val remainingMinutes = minutes % 60
+const val TIME = 60
 
-    val hoursFormatted = hours.toString().padStart(2, '0')
-    val minutesFormatted = remainingMinutes.toString().padStart(2, '0')
-    val secondsFormatted = remainingSeconds.toString().padStart(2, '0')
+fun main() {
+
+    val seconds: Int = 6480
+    val minutes: Int = seconds / TIME
+    val remainingSeconds: Int = seconds % TIME
+    val hours = minutes / TIME
+    val remainingMinutes = minutes % TIME
+
+    val hoursFormatted = String.format("%02d", hours)
+    val minutesFormatted = String.format("%02d", remainingMinutes)
+    val secondsFormatted = String.format("%02d", remainingSeconds)
 
     println("$hoursFormatted:$minutesFormatted:$secondsFormatted")
 }
